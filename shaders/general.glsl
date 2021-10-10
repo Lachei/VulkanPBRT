2d3e5f92664170657bed5c9206fc9139633d3635
Josef Stumpfegger <josefstumpfegger@outlook.de>
2021-10-10 14:28:07 +0200

    Max radiance reduced for firefly suppression, unnesecary reflection calculation removed

--- shaders/general.glsl
+++ shaders/general.glsl
@@ -7,7 +7,7 @@ const float RECIPROCAL_PI2 = 0.15915494;
 const float EPSILON = 1e-6;
 const float BLEND_ALPHA = .2f;
 const float c_MinRoughness = 0.04;
-const float c_MaxRadiance = 1e2;
+const float c_MaxRadiance = 1e1;
 const float c_MinTermination = 0.05;
 
 float pow2(float f){
@@ -328,8 +328,6 @@ float microfacetDistribution(PBRInfo pbrInputs)
 vec3 BRDF(vec3 v, vec3 l, vec3 h, SurfaceInfo s)
 {
     vec3 n = s.normal;
-    vec3 reflection = -normalize(reflect(v, n));
-    reflection.y *= -1.0f;
 
     float NdotL = clamp(dot(n, l), 0.001, 1.0);
     float NdotV = clamp(abs(dot(n, v)), 0.001, 1.0);
