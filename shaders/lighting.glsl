8e40b5cdd89cb5ecdf657c8a1fce76c113359b5f
Josef Stumpfegger <josefstumpfegger@outlook.de>
2021-12-08 10:09:35 +0100

    started to implement different light sampling methods.

--- shaders/lighting.glsl
+++ shaders/lighting.glsl
@@ -12,6 +12,7 @@ float powerHeuristics(float a, float b){
 //return light color(area foreshortening already included)
 //only light which can contribute are considered, priority sampling over light strength
 //uses weighted reservoir sampling to only have to go through the lights once
+#ifdef LIGHT_SAMPLE_SURFACE_STRENGTH
 vec3 sampleLight(vec3 pos, vec3 n, inout RandomEngine re, out vec3 l, out float pdf)
 {
   pdf = 0;
@@ -90,6 +91,55 @@ vec3 sampleLight(vec3 pos, vec3 n, inout RandomEngine re, out vec3 l, out float 
   traceRayEXT(tlas, gl_RayFlagsTerminateOnFirstHitEXT | gl_RayFlagsOpaqueEXT | gl_RayFlagsSkipClosestHitShaderEXT | gl_RayFlagsNoOpaqueEXT, 0xFF, 0, 0, 1, pos, tmin, l, tmax, 0);
   return pickedLightStrength * float(!shadowed);
 }
+
+#elif defined(LIGHT_SAMPLE_LIGHT_STRENGTH)
+#error "Not yet implemented"
+#else //uinform sampling of all light sources
+vec3 sampleLight(vec3 pos, vec3 n, inout RandomEngine re, out vec3 l, out float pdf){
+  float rand = randomFloat(re);
+  int lightInd = rand * infos.lightCount - int(rand); //ensures that all lights have the same probability and that lightIndex < infos.lightCount
+  vec3 lightStrength = lights.l[i].colAmbient.xyz + lights.l[i].colDiffuse.xyz + lights.l[i].colSpecular.xyz;
+  switch(int(lights.l[i].v0Type.w)){
+      case lst_directional:
+        d = distance(pos, lights.l[i].v0Type.xyz);
+        attenuation = 1.0f / (lights.l[i].strengths.x + lights.l[i].strengths.y * d + lights.l[i].strengths.z * d * d);
+        lightStrength *= dot(n, -lights.l[i].dirAngle2.xyz) * attenuation;
+        l = normalize(-lights.l[i].dirAngle2.xyz);
+        break;
+      case lst_point:
+        d = distance(pos, lights.l[i].v0Type.xyz);
+        attenuation = 1.0f / (lights.l[i].strengths.x + lights.l[i].strengths.y * d + lights.l[i].strengths.z * d * d);
+        lightStrength *= dot(n, normalize(lights.l[i].v0Type.xyz - pos)) * attenuation;
+        l = normalize(lights.l[i].v0Type.xyz - pos);
+        break;
+      case lst_spot:
+
+        break;
+      case lst_ambient:
+
+        break;
+      case lst_area:
+        //sample triangle position
+        vec2 barycentrics = sampleTriangle(randomVec2(re));
+        vec3 p1 = lights.l[i].v0Type.xyz;
+        vec3 p2 = lights.l[i].v1Strength.xyz;
+        vec3 p3 = lights.l[i].v2Angle.xyz;
+        vec3 lightP = blerp(barycentrics, p1, p2, p3);
+        vec3 lightDir = lightP - pos;
+        vec3 lightNormal = cross(p2 - p1, p3 - p1);
+        float triangleArea = .5f * length(lightNormal);
+        lightNormal = normalize(lightNormal);
+        d = length(lightDir);
+        lightDir /= d;
+        attenuation = 1.0f / (lights.l[i].strengths.x + lights.l[i].strengths.y * d + lights.l[i].strengths.z * d * d);
+        strength = max(dot(n, lightDir), 0) * max(dot(-lightDir, lightNormal), 0) * lightPower * attenuation * triangleArea;
+        lightStrength *= max(dot(n, lightDir), 0) * max(dot(-lightDir, lightNormal), 0) * attenuation * triangleArea;
+        curL = lightDir;
+        curTmax = d - tmin;
+        break;
+    }
+}
+#endif
 
 //calculates direct lighting on the surface point at pos
 vec3 nextEventEsitmation(vec3 pos, vec3 o, SurfaceInfo s, vec3 throughput, inout RandomEngine re){
