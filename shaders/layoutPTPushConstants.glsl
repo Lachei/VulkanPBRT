144a20c2e8c8f240e5b741c32246d48281d03155
Sven Lüpke <luepke@in.tum.de>
2021-12-10 17:42:33 +0100

    Added worldToViewNormals matrix to push constants

--- shaders/layoutPTPushConstants.glsl
+++ shaders/layoutPTPushConstants.glsl
@@ -6,6 +6,7 @@ layout(push_constant) uniform PushConstants
 	mat4 inverseViewMatrix;
 	mat4 inverseProjectionMatrix;
 	mat4 prevView;
+    mat4 worldToViewNormals;
 	uint frameNumber;
 	uint sampleNumber;
 } camParams;
