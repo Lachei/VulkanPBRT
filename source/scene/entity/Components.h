baefb004e004a6033212708352d2853a910e2741
Sven Lüpke <luepke@in.tum.de>
2022-01-21 16:24:14 +0100

    Added more component structs

--- source/scene/entity/Components.h
+++ source/scene/entity/Components.h
@@ -1,9 +1,35 @@
 #pragma once
 #include "SimpleMath.h"
+#include "behaviors/Base.h"
 
 struct Transform
 {
     DirectX::SimpleMath::Quaternion orientation;
     DirectX::SimpleMath::Vector3 position;
     DirectX::SimpleMath::Vector3 scale;
+};
+
+struct Camera
+{
+    float focal_length;
+    float near_clip;
+    float far_clip;
+};
+
+struct Material
+{
+    DirectX::SimpleMath::Vector3 albedo;
+    float roughness;
+    float metalness;
+    // TODO: figure out a better material model that includes textures
+};
+
+struct Mesh
+{
+    
+};
+
+struct Behavior
+{
+    EntityBehaviorBase* entity_behavior;
 };
--- source/scene/entity/Entity.h
+++ source/scene/entity/Entity.h
@@ -11,7 +11,7 @@ public:
     uint64_t GetId() const;
 
     /**
-     * \brief Get an component that is attached to this entity.
+     * \brief Get a component that is attached to this entity.
      * \tparam T The type of the component which should be returned.
      * \return Pointer to the component. Can be nullptr if this entity does not have a component of type T.
      */
