d03490bacf47976938a940e68de6149458d2ad70
Sven Lüpke <luepke@in.tum.de>
2022-03-03 19:46:50 +0100

    Reverted more files to master

--- source/buffers/GBuffer.hpp
+++ source/buffers/GBuffer.hpp
@@ -6,21 +6,20 @@
 // class holding all references for the gbuffer
 // supports automatically updating the ray tracing descriptor set
 // care to update also the RayTracingVisitor.hpp descriptor layout if you add things to the gbuffer
-class GBuffer : public vsg::Inherit<vsg::Object, GBuffer>
-{
+class GBuffer: public vsg::Inherit<vsg::Object, GBuffer>{
 public:
     GBuffer(uint32_t width, uint32_t height);
 
     uint32_t width, height;
     vsg::ref_ptr<vsg::DescriptorImage> depth, normal, material, albedo;
 
-    void update_descriptor(vsg::BindDescriptorSet* desc_set, const vsg::BindingMap& binding_map);
+    void updateDescriptor(vsg::BindDescriptorSet* descSet, const vsg::BindingMap& bindingMap);
 
-    void update_image_layouts(vsg::Context& context);
+    void updateImageLayouts(vsg::Context& context);
 
     void compile(vsg::Context& context);
 
 protected:
-    vsg::ref_ptr<vsg::Sampler> _sampler;
-    void setup_images();
+    vsg::ref_ptr<vsg::Sampler> sampler;
+    void setupImages();
 };
