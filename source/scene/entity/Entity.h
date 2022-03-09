1850b00269f9416e7cbaf1399ad9601bedb8ca01
Sven Lüpke <luepke@in.tum.de>
2022-03-09 21:56:51 +0100

    Removed GetId function from Entity class

--- source/scene/entity/Entity.h
+++ source/scene/entity/Entity.h
@@ -9,9 +9,6 @@ class Entity
 {
 public:
     Entity(uint64_t entityId, EntityManager& entity_manager);
-
-    uint64_t GetId() const;
-
     /**
      * \brief Get a component that is attached to this entity.
      * \tparam T The type of the component which should be returned.
@@ -29,10 +26,6 @@ private:
 inline Entity::Entity(uint64_t entityId, EntityManager& entity_manager)
     : _id(entityId), _entity_manager(entity_manager)
 {
-}
-inline uint64_t Entity::GetId() const
-{
-    return _id;
 }
 template <typename T>
 T* Entity::GetComponent()
